3b57cdf5d067b1e25639a77abba84b40a558d3db
Ahmad Azzam Hafidz <103403778+ahmadazzam22607@users.noreply.github.com>
2023-08-02 15:37:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript code to manipulate the DOM and add interactivity
+
+// Example: Changing the header text when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header h1");
+  header.textContent = "Welcome to Your Interactive Website";
+});
+
+// Example: Adding a click event listener to a button
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("#click-me-button");
+  button.addEventListener("click", function () {
+    alert("You clicked the button!");
+  });
+});
