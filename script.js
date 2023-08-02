b38761279d04277ee14438e30d38171effe3cf97
Ahmad Azzam Hafidz <103403778+ahmadazzam22607@users.noreply.github.com>
2023-08-02 15:45:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Example: Changing the header text when the page loads
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector("header h1");
-  header.textContent = "Welcome to Your Interactive Website";
+  header.textContent = "Welcome to My Interactive Website";
 });
 
 // Example: Adding a click event listener to a button
